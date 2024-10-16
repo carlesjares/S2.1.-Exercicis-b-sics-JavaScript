7c791ec85bb0460b42e787cf71644ad6468a3610
Carles Jares <carlesjaresalbareda@gmail.com>
2024-10-16 12:19:14 +0200

    terminado exercicis 1.1 arrows function

--- Exercici 1.1 Arrow functions/main.js
+++ Exercici 1.1 Arrow functions/main.js
@@ -2,29 +2,51 @@
 
 //Exercici 1 
 
-const add = (a, b) => a+ b;
+// let add = (a, b) => a+ b;
+
+// console.log(add(3, 5)); // Sortida: 8
 
 // Exercici 2
 
-const randomNumber = () => Math.random() * 100;
+// let randomNumber = () => Math.floor(Math.random() * 101);
 
-document.getElementById('number').textContent = randomNumber().toFixed(2);
+// console.log(randomNumber(56));
 
 // Exercici 3 
 
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
 
-  greet = () => {
-    console.log(`Hola, ${this.name}`);
-  }
-}
+//   greet = () => {
+//     console.log(`Hola, ${this.name}`);
+//   }
+// }
 
-const persona = new Person('Joan');
-persona.greet();  // Sortida: Hola, Joan
+// let persona = new Person('Joan');
+// persona.greet();  // Sortida: Hola, Joan
 
 
+
+// NIVELL 2
+
+//EXERCICI 4
+
+// let printNumbers = (numbers) => {
+//   for (let i = 0; i < numbers.length; i++) {
+//     (() => console.log(numbers[i]))();  // Funció de fletxa dins del bucle
+//   }
+// };
+
+// let nums = [10, 20, 30, 40, 50];
+// printNumbers(nums);
+
+// NIVELL 3
+//EXERCICI 5 
+
+// setTimeout(() => {
+//   console.log("Retrasado por 3 segundo.");
+// }, 3000);
 
 
