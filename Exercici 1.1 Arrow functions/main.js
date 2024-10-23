a0b0e4bcf696a4d5b6ad059de18500496bc4ec66
Carles Jares <carlesjaresalbareda@gmail.com>
2024-10-23 10:46:08 +0200

    ARRAY LOPS NIVELL 2 , exercici 5

--- Exercici 1.1 Arrow functions/main.js
+++ Exercici 1.1 Arrow functions/main.js
@@ -384,13 +384,27 @@
 //  Utilitza un bucle for-in per a imprimir 
 // a la consola cada clau i el seu valor corresponent.
 
-let obj = {
-    nom : 'ona',
-    edat :  25,
-    ciutat : 'Barcelona'
+// let obj = {
+//     nom : 'ona',
+//     edat :  25,
+//     ciutat : 'Barcelona'
 
-}
+// }
 
-for (let clau in obj) {
-    console.log(clau + " : " + obj[clau]);
+// for (let clau in obj) {
+//     console.log(clau + " : " + obj[clau]);
+// }
+
+// Exercici 5
+// for-of amb break: Teniu una array de números. Utilitza un bucle for-of per a imprimir a la consola
+//  els números fins a trobar el número 5, llavors atura el bucle: let numeros = [1, 2, 3, 4, 5, 6];
+
+let numeros = [1, 2, 3, 4, 5, 6] ;
+
+for (const numero of numeros){
+    if (numero === 5){
+        break;
+        
+    }
+    console.log(numero);
 }
